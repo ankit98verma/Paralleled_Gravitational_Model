7535c8107c436c93c09975b3a0c047e1b6303b3c
Garima <garima.15121@gmail.com>
2020-05-25 16:31:03 -0700

    Heat Map

---
+++ Code/utilities/heat_map.m
@@ -0,0 +1,53 @@
+%Draw generate heat map from lat, long maps
+clear all
+clc
+close all
+
+
+fileID = fopen('output_potential.mat','r');
+formatSpec = '%f %f %f %f %f';
+sizeA = [5 Inf];
+A = fscanf(fileID,formatSpec, sizeA);
+fclose(fileID);
+
+% Convert XYZ to Lat-Long
+for i=1:1:length(A)
+   lat(i) = asin(A(4,i));
+   long(i) = atan2(A(3,i), A(2,i));
+   potential(i) = A(5,i);
+end
+
+%% Generate 2D scatter plot
+figure(1)
+title('Geopotential model');
+hold on
+grid on
+xlabel('Longitude (deg)');
+ylabel('Latitude (deg)');
+pointsize = 15;
+scatter(long*180/pi, lat*180/pi, pointsize, potential,'filled')
+colorbar;
+saveas(gcf, '../results/heatmap_2.png');
+
+
+%% Generate 3D scatter plot
+figure(1)
+title('Geopotential model');
+hold on
+grid on
+xlabel('x');
+ylabel('y');
+zlabel('z');
+pointsize = 15;
+scatter3(A(2,:),A(3,:),A(4,:), pointsize, potential,'filled')
+colorbar;
+
+hold on
+X_axis = [ 0, 0, 0; 1.2, 0, 0];
+Y_axis = [ 0, 0, 0; 0, 1.2, 0];
+Z_axis = [ 0, 0, 0; 0,0, 1.2];
+plot3(X_axis(:,1), X_axis(:,2), X_axis(:,3), 'Linewidth' ,3);
+plot3(Y_axis(:,1), Y_axis(:,2), Y_axis(:,3), 'Linewidth' ,3);
+plot3(Z_axis(:,1), Z_axis(:,2), Z_axis(:,3), 'Linewidth' ,3);
+view(120,25);
+saveas(gcf, '../results/heatmap_3.png');
--- Code/utilities/visulaize_icosphere.m
+++ Code/utilities/visulaize_icosphere.m
@@ -5,34 +5,23 @@ format long;
 set(0,'defaultAxesFontSize', 14);
 set(0, 'DefaultLineLineWidth', 2);
 
-%%
 epsilon = 1e-6;
-%%      
-vertices = readtable('vertices.csv');  % skips the first three rows of data
+%% vertices = readtable('vertices.csv');  % skips the first three rows of data
 edges = readtable('cpu_edges.csv');
 vertices_sph = readtable('vertices_sph.csv');
 x = vertices.x;
 y = vertices.y;
 z = vertices.z;
-%%
-% histogram of lats
-% figure(1);  
-% histogram(vertices_sph.theta, ceil(2*pi/1e-6));
-% figure(2);
-plot(vertices_sph.theta);
-%%
-% plotting vertices
+%% plotting vertices
 figure(3);
-% [sx, sy, sz] = sphere(100);
-% surface(sx, sy, sz, 'FaceColor', 'none', 'EdgeColor', 'k');
 hold on;
-plot3(x, y, z, 'r*');
+% plot3(x, y, z, 'r*');
 
-%%
-% plotting edeges
+%% plotting edeges
 for i = 1:height(edges)
     x_tmp = [edges.x1(i), edges.x2(i)];
     y_tmp = [edges.y1(i), edges.y2(i)];
     z_tmp = [edges.z1(i), edges.z2(i)];
     plot3(x_tmp, y_tmp, z_tmp, 'b');
 end
+saveas(gcf, '../results/icosphere.png');
